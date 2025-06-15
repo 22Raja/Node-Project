9fe9ffedd2211ec33420198c0d3804b8b393b015
Keerthi raja <105999990+22Raja@users.noreply.github.com>
2025-06-15 21:35:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,33 @@ const app = express();
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://keerthiraja228:<db_password>@library.cblol5i.mongodb.net/?retryWrites=true&w=majority&appName=library";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const uri = "mongodb+srv://keerthiraja228:MfmDkBO42DV1zql2@people-club.sol6thw.mongodb.net/yourDatabaseName?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
